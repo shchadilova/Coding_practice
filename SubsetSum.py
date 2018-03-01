47dc1f0089882ffa756e00bc4d8b14555b487cca
Yulia Shchadilova <shchadilova@g.harvard.edu>
2018-03-01 17:56:11 -0500

    upd 2
    still too slow

--- SubsetSum.py
+++ SubsetSum.py
@@ -1,25 +1,21 @@
 import sys
 
-def SubsetSum(a, i , n_i, sum, _cashe={}):
-        
-        memokey = str((i+1,sum))
-        
-        if memokey in _cashe.keys():
-            return _cashe[memokey]
-        else:
-            if sum < 0 or i>=n_i:
-                return False
-            elif sum == 0:
-                return True
-            elif a[i] == sum:
-                return True
-            elif SubsetSum(a,i+1,n_i,sum) == True:
-                return True
-            elif SubsetSum(a,i+1,n_i,sum-a[i]) == True:
-                return True
-            else:
-                return False
+
+def SubsetSum(a, n_i , s, sum):
     
+    n_s = sum + 1
+    
+    #Now define the table for the DP aplgorithm
+    d = [[False] * n_s for i in range(n_i)]
+    
+    for i in range(n_i):
+        for s in range(n_s):
+            if i == 0:
+                d[i][s] = (a[i] == s)
+            else:
+                d[i][s] = (d[i-1][s]) or (a[i] == s) or (d[i-1][s-a[i]])
+                    
+    return d[n_i-1][-1]
 
 n = int(input().strip())
 for i in range(n):
@@ -28,8 +24,7 @@ for i in range(n):
     if sum(a) % 2 == 1:
         print("NO")
     else:
-        if SubsetSum(a, 0 , m , sum(a)/2) == True:
+        if SubsetSum(a, len(a) , i , int(sum(a)/2)) == True:
             print("YES")
         else:
             print("NO")
-
