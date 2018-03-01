70dcfe0397e1c27aaef98e377a0f9d7c90c3aca8
Yulia Shchadilova <shchadilova@g.harvard.edu>
2018-03-01 17:55:47 -0500

    upd

--- SubsetSum.py
+++ SubsetSum.py
@@ -1,57 +1,35 @@
 import sys
 
-def SubsetSum(a, n_i):
-    
-    a_sum = sum(a)
-
-    if a_sum % 2 == 1:
-        return False
-    
-    else:
+def SubsetSum(a, i , n_i, sum, _cashe={}):
         
-        #Sum of the subsequence
-        ts = int(a_sum/2)
+        memokey = str((i+1,sum))
         
-        # Derine the boundaries of possible summations
-        # A <= s <= B
-        
-        a.sort()        
-        A = 0
-        B = 0
-        
-        for j in range(len(a)):
-            if a[j] < 0:
-                A += a[j]
+        if memokey in _cashe.keys():
+            return _cashe[memokey]
+        else:
+            if sum < 0 or i>=n_i:
+                return False
+            elif sum == 0:
+                return True
+            elif a[i] == sum:
+                return True
+            elif SubsetSum(a,i+1,n_i,sum) == True:
+                return True
+            elif SubsetSum(a,i+1,n_i,sum-a[i]) == True:
+                return True
             else:
-                B += a[j]
-        if ts < A or ts > B:
-            return False
-        
-        n_s = abs(A) + B + 1
-        
-        #Now define the table for the DP aplgorithm
-        d = [[0] * n_s for i in range(n_i)]
-        
-        for i in range(n_i):
-            for s in range(n_s):
-                if i == 0:
-                    if a[i] == s:
-                        d[i][s] = True
-                    else:
-                        d[i][s] = False
-                else:
-                    if d[i-1][s] == True or a[i] == s or d[i-1][s-a[i]] == True:
-                        d[i][s] = True
-                    else:
-                        d[i][s] = False
+                return False
     
-    return d[n_i-1][ts]
 
 n = int(input().strip())
 for i in range(n):
     m = int(input().strip())
     a = [ int(num) for num in input().strip().split(" ")]
-    if SubsetSum(a, m) == True:
-        print("YES")
-    else:
+    if sum(a) % 2 == 1:
         print("NO")
+    else:
+        if SubsetSum(a, 0 , m , sum(a)/2) == True:
+            print("YES")
+        else:
+            print("NO")
+
